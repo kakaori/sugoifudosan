2edc5ed41323f288ffc8ddd7f7c943a2f8bb52c0
kakaori <kaoneko6622@gmail.com>
2024-09-24 13:37:59 +0900

    logout etc

--- resources/views/partner/dashboard.blade.php
+++ resources/views/partner/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-partner-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('パートナー登録 Dashboard') }}
@@ -15,4 +15,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-partner-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Partner\AuthenticatedSessionController;
-use App\Http\Controllers\Partner\RegisteredUserController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,26 +18,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/* 管理者用ルーティング */
-Route::group(['prefix' => 'partner'], function () {
-    // 登録
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('partner.register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    // ログイン
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('partner.login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    // 以下の中は認証必須のエンドポイントとなる
-    Route::middleware(['auth:partner'])->group(function () {
-        // ダッシュボード
-        Route::get('dashboard', fn() => view('partner.dashboard'))
-            ->name('partner.dashboard');
-    });
+Route::middleware(['auth:partner'])->group(function () { 
+    Route::get('/partner/profile', [ProfileController::class, 'edit'])->name('partner.profile.edit');
+    Route::patch('/partner/profile', [ProfileController::class, 'update'])->name('partner.profile.update');
+    Route::delete('/partner/profile', [ProfileController::class, 'destroy'])->name('partner.profile.destroy');
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/partner.php';
